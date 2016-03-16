69a60f7bee04ac662eea030b90ccc4ce2467bc10
User <user@macbook-air-user-3.local>
2016-03-16 22:02:03 +0400

    Initial Commit

---
+++ LoginApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginApp
+//
+//  Created by User on 3/16/16.
+//  Copyright © 2016 Lilit Khachatryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoginApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LoginApp
+//
+//  Created by User on 3/16/16.
+//  Copyright © 2016 Lilit Khachatryan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
